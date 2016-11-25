ce3e2ce9047e442ec847534cd147ec8dba8e0c20
emilysr <emily.rasmussen@live.dk>
2016-11-25 16:47:38 +0100

    Update Main.java

--- CDIO3_4/src/main/Main.java
+++ CDIO3_4/src/main/Main.java
@@ -5,7 +5,8 @@ import controller.Spil;
 public class Main {
 
 	public static void main(String[] args){
-		
+
+// Kalder en metode der kører spillet		
 		Spil spil= new Spil();
 		spil.runSpil();
 	}
