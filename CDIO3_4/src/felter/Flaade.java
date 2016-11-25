46b94f045d0f26a47efb39c65c24ee8a8dda1456
SimonChr <simonchristiansen1@gmail.com>
2016-11-25 15:38:53 +0100

    Update Flaade.java

--- CDIO3_4/src/felter/Flaade.java
+++ CDIO3_4/src/felter/Flaade.java
@@ -1,6 +1,12 @@
+
 package felter;
 
+import java.awt.Color;
+
+import desktop_resources.GUI;
+import entity.Mui;
 import entity.Spiller;
+import sprog.Tekst;
 
 public class Flaade extends Ejerskab{
 
@@ -8,15 +14,23 @@ public class Flaade extends Ejerskab{
 	final private int leje2 = 1000;
 	final private int leje3 = 2000;
 	final private int leje4 = 4000;
+	
+	int pris;
+	
+	public Color farve = Color.GRAY;
 
 	public Flaade(String feltType, int pris){
 		super(feltType, pris);
-
+		this.pris = pris;
 
 	}
 	public int getLeje(){
+		
+		int leje = 0;
+		
+		if(super.getEjer() != null){
+			
 		int antalFlaade = super.getEjer().getAntalFlaade();
-		int leje;
 
 		switch(antalFlaade){
 		case 1: 
@@ -32,23 +46,63 @@ public class Flaade extends Ejerskab{
 			leje = leje4;
 			break;
 		default: leje = 0;
-		}
+		}}
+		
 		return leje;
 
 	}
 
 
 	@Override
-	public void landPaaFelt(Spiller spiller) {
-		
-
-	}
+	public void landPaaFelt(Spiller spiller, Mui mui) {
+		mui.midtBeskrivelse(Tekst.toString(39));
+	if(this.ejer == null){
+		if(spiller.getKonto().pengeNok(getPris())){
+			if(mui.getToKnap(Tekst.toString(14), Tekst.toString(19), Tekst.toString(20)) == true){
+				mui.midtBeskrivelse(Tekst.toString(76) + getPris());	
+				koebFelt(spiller);
+				GUI.setBalance(spiller.getNavn(), spiller.getBalance());
+			}	
+		else{
+				mui.midtBeskrivelse(Tekst.toString(77));
+				}}
+		else{
+				mui.midtBeskrivelse(Tekst.toString(21));
+			
+		}}
+	else{	
+			if(this.ejer == spiller){
+				mui.midtBeskrivelse(Tekst.toString(16));
+			}
+			else{
+		mui.midtBeskrivelse(Tekst.toString(17));
+		mui.getEnKnap(Tekst.toString(22), Tekst.toString(7));
+		mui.midtBeskrivelse(Tekst.toString(68) + getLeje());
+		spiller.aendreBalance(-getLeje());
+		this.ejer.aendreBalance(getLeje());
+		GUI.setBalance(spiller.getNavn(), spiller.getBalance());
+		GUI.setBalance(ejer.getNavn(), ejer.getBalance());
+			}}}
 
 	public void koebFelt (Spiller spiller) {
 		super.koebFelt(spiller);
 		spiller.setAntalFlaade();
 	}
+	@Override
+	public String subtekst() {
+		String subtekst = "500, 1000, 2000, 4000";
+		return subtekst;
+	}
+	@Override
+	public String getbeskrivelse() {
 
+		return Tekst.toString(69) + Tekst.toString(67) + 4000 + Tekst.toString(73);
+	}
 
+	@Override
+	public Color getColor() {
+		return farve;
+	}
+	
 
 }
