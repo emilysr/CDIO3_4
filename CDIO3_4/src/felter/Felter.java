d4a37870327607213bc430883d2e4a1af19751f7
SimonChr <simonchristiansen1@gmail.com>
2016-11-25 16:58:48 +0100

    Update Felter.java

--- CDIO3_4/src/felter/Felter.java
+++ CDIO3_4/src/felter/Felter.java
@@ -8,21 +8,24 @@ import entity.Konto;
 import entity.Mui;
 import entity.Terning;
 
+// abstract felt, alle felter arver fra Felter
 public abstract class Felter {
 	
 	public Color farve;
 
 	private String feltType;
 	
+// Konstruktør
 	public Felter (String type) {
 		this.feltType = type;
 	}
 	
+// getType retunere felttypen
 	public String getType() {
 		return feltType;
 	}
 	
-	
+// abstracte metoder der anvendes i alle ikke abstracte felter
 	abstract public void landPaaFelt (Spiller spiller, Mui mui);
 	
 	abstract public String subtekst();
